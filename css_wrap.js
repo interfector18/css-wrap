0cfb6d6ca5d1728c378eb44441b4faf6afbfa249
interfector18 <zheeththedragon@gmail.com>
2020-04-22 20:25:20 +0200

    Add multiple wraps option

--- css_wrap.js
+++ css_wrap.js
@@ -14,37 +14,46 @@
  *
  */
 var
-  path = require( 'path' ),
-  fs = require( 'fs' ),
-  deepmerge = require( 'deepmerge' ),
-  css_parse = require( 'css-parse' ),
-  css_stringify = require( 'css-stringify' ),
-  processRules = function( list, options ) {
-    return list.map( function( r ) {
-      if ( r.selectors ) {
-        r.selectors.forEach( function( s, index ) {
+  path = require('path'),
+  fs = require('fs'),
+  deepmerge = require('deepmerge'),
+  css_parse = require('css-parse'),
+  css_stringify = require('css-stringify'),
+  processRules = function (list, options) {
+    return list.map(function (r) {
+      if (r.selectors) {
+        r.selectors.forEach(function (s, index) {
           if (options.skip && options.skip.test(s)) return
-          var selector = options.selector ? options.selector + " " + s : s;
-          r.selectors[ index ] = selector;
+          var selector = '';
+          if (typeof options.selector === 'object' && options.selector.length) {
+            var selectors = [];
+            options.selector.forEach((sel) => selectors.push(sel ? sel + " " + s : s));
+            selector = selectors.join(", ");
+          }
+          else {
+            selector = options.selector ? options.selector + " " + s : s;
+          }
+          r.selectors[index] = selector;
         });
       }
-      if ( r.type === "media" ) {
-        r.rules = processRules( r.rules, options );
+      if (r.type === "media") {
+        r.rules = processRules(r.rules, options);
       }
       return r;
     });
   },
-  css_wrap = function( string, options ) {
-    options = deepmerge({
-      // Defaults
-      selector: ".css-wrap",
-      skip: null
-    }, options || {});
+  css_wrap = function (string, options) {
+    if (!options)
+      options = { selector: '.css-merge', skip: null };
+    if (!options.selector)
+      options.selector = '.css-merge';
+    if (!options.skip)
+      options.skip = null;
     if (fs.existsSync(path.resolve(string))) {
       string = fs.readFileSync(string).toString();
     }
-    var css = css_parse( string );
-    css.stylesheet.rules = processRules( css.stylesheet.rules, options );
-    return css_stringify( css );
+    var css = css_parse(string);
+    css.stylesheet.rules = processRules(css.stylesheet.rules, options);
+    return css_stringify(css);
   };
 module.exports = css_wrap;
